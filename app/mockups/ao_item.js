4dfa91cf1ef88d3b222f5bfe3f57b14aea4312dc
Mateusz.Kasiuba <Mateusz.Kasiuba@jisc.ac.uk>
2016-01-22 13:31:25 +0000

    add mockup for single AO

---
+++ app/mockups/ao_item.js
@@ -0,0 +1,22 @@
+{
+    "ao_id": 5,
+    "ao_title": "Test AO",
+    "ao_apc_status": {
+        "status_code": 1,
+        "status_value": "Approved",
+        "date": "12/01/16",
+        "status_reason": "Some reason"
+    },
+    "ao_assigned_to": "Latimer Hazal",
+    "ao_route": "gold",
+    "ao_submitted": {
+        "status": false,
+        "date": false,
+        "note": "Some note"
+    },
+    "ao_publisher": {
+        "status": "Accepted",
+        "date": "12/01/16",
+        "note": "Some note"
+    }
+}
