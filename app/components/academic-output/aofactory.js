cb0f5bb95e884149138c5894fd71a03e8ce02b4c
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2016-01-25 15:01:06 +0000

    Tweaked the sample JSON data. Collections should be [{},{}] not keyed objects (i.e. Not {0: {}, 1:{}}
    Any fields that are not simple types like strings or integers will be
    linked to objects from the database and will therefore have ids.
    
    Attributes are lower case i.e "id" not "ID"

--- app/components/academic-output/aofactory.js
+++ app/components/academic-output/aofactory.js
@@ -1,41 +1,96 @@
 'use strict';
 
 define(
-  function () {
-    console.log ("Create a factory!");
-    return angular.module('aofactory', ['ui.router'])
-    .factory('AOStorage', function() {
+    'aofactory',// Name the Javascript module.
+    ['academic-output'],
+    function () {
+      console.log ("Create a factory!");
+      return angular.module('academic-output')
+      .factory('AOStorage', function() {
         var storage = {
-          AOData : [{
-          	ref : '9584',
-        	title : 'Diagnosis and management of primary cliairy dyskinetia',
-        	status: 'Accepted',
-        	reason: '',
-        	route : 'Gold',
-        	awards : {NEGO16003457 : 'NE/GO16003/457', NEGO89803090 :'NE/GO89803/090'},
-        	administrator: 'Latimer Hazar'
-          }]
+            AOData : {
+              id : 9584,
+              name : 'Diagnosis and management of primary cliairy dyskinetia',
+              status: {id : 2, value: 'Accepted'},
+              reason: '',
+              route : {id : 3, value: 'Gold'},
+              awards : [{
+                id : 32,
+                ref : "NEGO16003457",
+                value : 'NE/GO16003/457'
+              },{
+                id : 37,
+                ref : "NEGO89803090",
+                value : 'NE/GO89803/090'
+              }],
+              administrator: {
+                id : 653,
+                name : 'Latimer Hazar'
+              }
+            },
+            AOList : [{
+              id : 1234,
+              name : 'Some Academic article',
+              authors : [{
+                role : {
+                  id : 234,
+                  value : "Corresponding Author"
+                },
+                author : {
+                  id : 754,
+                  name : 'Mateusz Kasiuba'
+                }
+              }],
+              publication : {
+                id : 23,
+                name : 'Non exist Journal',
+              },
+              publisher : {
+                id : 678,
+                name : 'famous publisher',
+              },
+              cost : 9999,
+              status : {id : 35, value: 'Unpublished'},
+              payment : {id : 36, value: 'Unpaid'}
+            },{
+              id : 9999,
+              name : 'Special Article',
+              authors : [{
+                id : 23,
+                title : 'Famous Author'
+              }],
+              publication : {
+                id : 23,
+                name : 'Non exist Journal',
+              },
+              publisher : {
+                id : 450,
+                name : 'small publisher',
+              },
+              cost : 123,
+              status : {id : 37, value: 'Published'},
+              payment : {id : 38, value: 'Unpaid'}
+            }]
         };
-        
+
         var deleteAward = function(key){
-        	delete(storage.AOData['awards'][key]);
-    		console.log( scope['academicOutput']['awards']);
-        }
-        
-        var addAward = function(award){
-        	storage.AOData['awards'][Date.now()] = award;
-            console.log(storage.AOData['awards']);
-        }
-        
+          delete(storage.AOData['awards'][key]);
+        };
+
+        var addAward = function(){
+          storage.AOData['awards'][Date.now()] = '';
+          console.log(storage.AOData['awards']);
+        };
+
         var getStorage = function(){
-        	return storage;
-        }
-        
+          return storage;
+        };
+
         return {
-            deleteAward: deleteAward,
-            addAward: addAward,
-            getStorage: getStorage
-          };
-        });
-  }
+          deleteAward: deleteAward,
+          addAward: addAward,
+          getStorage: getStorage
+        };
+      });
+    }
 );
