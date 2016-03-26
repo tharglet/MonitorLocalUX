3731cd0b0d8addb327b29eec968895198e6845de
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2016-03-26 13:47:29 +0000

    Refdata directive now working properly. Now to extend to lookup other components.

--- app/components/grails/DirectiveRefdata.js
+++ app/components/grails/DirectiveRefdata.js
@@ -3,42 +3,72 @@
 define (
   function () {
     
-    // Personal Controller for this Directive.
-    var refdataLinking = function ($scope) {
-      
-      // Add the data and update functions here.
-      $scope.data = [];
-      
-      $scope.refresh = function () {
-        
-        var funcName = $scope.property + "Values";
-        
-        if ( typeof $scope.object[funcName] === 'function') {
-          // Grab the values...
-          ($scope.object[funcName])().then(function( rdata ){
-            $scope.data = rdata.data;
-          });
-        } 
-      };
-      
-      // Refresh the list driving the refdata select when the object reference changes only.
-      $scope.$watch("object", function(newVal, oldVal, $scope){
-        $scope.refresh();
-      });
-    };
-    
-    return function () {
+    return function ($compile, $templateRequest) {
       
       console.log ("Adding directive");
       
       return {
         restrict: 'E',
-        link: refdataLinking,
         scope: {
-          "object": "=",
-          "property": "@",
+          object:   "=",
+          property: "@"
         },
-        templateUrl: 'components/grails/directive-partials/refdata.html',
+        compile: function compile(tElem, tAttr, transclude) {
+          
+          // The linknig function. Needs to be declared here to have access to the template
+          // request service.
+          return function ($scope, iElem, iAttr) {
+            
+            // Add the data and update functions here.
+            $scope.data = [];
+            
+            // The refresh function
+            $scope.refresh = function () {
+              
+              var funcName = $scope.property + "Values";
+              
+              if ( typeof $scope.object[funcName] === 'function') {
+                // Grab the values...
+                ($scope.object[funcName])().then(function( rdata ){
+                  $scope.data = rdata.data;
+                });
+              } 
+            };
+            
+            // Refresh the list driving the refdata select when the object reference changes only.
+            $scope.$watch("object", function(newVal, oldVal, $scope){
+              $scope.refresh();
+            });
+
+            
+            $templateRequest("components/grails/directive-partials/refdata.html").then(function(html){
+              var template = angular.element(html);
+              
+              // Grab each element.
+              angular.forEach(tAttr, function(val, name) {
+                if (!name.startsWith('$')) {
+                  switch ( name ) {
+                    case "object":
+                    case "property":
+                      // ignore these 2.
+                      break;
+                    case "class":
+                      // Add classes rather than setting.
+                      template.addClass(val);
+                      break;
+                    default:
+                      // Copy the value over.
+                      template.attr(name, val);
+                  }
+                }
+              });
+              $compile(template)($scope);
+              
+              // Replace
+              iElem.replaceWith(template);
+            });
+          };
+        },
       };
     };
   }
--- app/components/grails/main.js
+++ app/components/grails/main.js
@@ -10,13 +10,19 @@ define (
   ['./component-edit-ctrl', './DirectiveRefdata', 'angular-resource', './resource-manager' ],
   function(ComponentEditController, RefdataDirective) {
     
+    // Define some prefixes and var names here.
     var directiveNamespace = "kint";
     var grailsResourcePriovderName = 'grailsResource';
     
+
+    // The module and the service.
     var ngGr = angular.module('grails', ['ngResource']);
     ngGr.service('GrailsService', ['$resource', '$http', '$q', 'appConfig', ResourceManager]);
+    
+    // The functions for the below are stored in different files. They are included above using
+    // requirejs and they should return a method. That method can then be accessed by applied var name.
     ngGr.controller('GrailsEditController', ['$rootScope', '$scope', '$state', '$stateParams', '$timeout', grailsResourcePriovderName, ComponentEditController]);
-    ngGr.directive(directiveNamespace + 'Refdata', [RefdataDirective]);
+    ngGr.directive(directiveNamespace + 'Refdata', ['$compile', '$templateRequest', RefdataDirective]);
     
     // If the UI Router is present then we should add a decorator to allow for,
     // GrailsResource resolution.
@@ -24,13 +30,15 @@ define (
       if($injector.has('$stateProvider')){
         var $stateProvider = $injector.get('$stateProvider');
         
-        // Lets add a lazy dependencies decorator to the state provider.
+        // Custom decorator to allow us to define Grails resource type directly on the state definition.
+        // The resource will be resolved before the state is rendered and the resource made available for
+        // injection using the name set in 'grailsResourcePriovderName' above.
         $stateProvider.decorator(grailsResourcePriovderName, function (state) {
           
+          // Check if the state
           if (state[grailsResourcePriovderName]) {
             var typeName = state[grailsResourcePriovderName];
             var depsResolutionKey = grailsResourcePriovderName;
-            
             
             // Add the resolve object if necessary.
             if (!state.resolve) {
