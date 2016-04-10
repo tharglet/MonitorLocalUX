e7bc976f00cf2f1356e649606d936b7fd917624e
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2016-04-10 12:14:19 +0100

    Some consistency in forms. Holding off on k-int templates for now.

--- app/components/kint-ui/directives/DrtvInput.js
+++ app/components/kint-ui/directives/DrtvInput.js
@@ -6,7 +6,19 @@ define (
     var camelToDash = function (str) {
       return str.replace(/\W+/g, '-')
       .replace(/([a-z\d])([A-Z])/g, '$1-$2');
-    }
+    };
+    
+    var extractObject = function (str) {
+      return str.replace(/(.+)(\[[^\]]+\]|\..+)$/, "$1");
+    };
+
+    var extractProperty = function (str) {
+      var res = str.replace(/\[["']([^"']+)["']\]$/, "$1");
+      if (!res || res == str) {
+        name = str.replace(/.*\.([^\.|\]]+)$/, "$1");
+      }
+      return res;
+    };
     
     var addAttributes = function(atrs, template) {
       angular.forEach(atrs, function(val, name) {
@@ -43,107 +55,135 @@ define (
     // Columns.
     var cols;
     
-    // Template type.
-    var templateType;
+    // Default template is to use the inputs.
+    var templateType = "input";
     
     return function (theme, $templateRequest, $compile) {
       return {
         restrict: 'E',
         require: '^^form',
         scope : false,
-        compile: function( tElem, tAttr ) {
-          
-          // Generate the name and label id from the supplied props.
-          name = tAttr['ngModel'].replace(/\[["']([^"']+)["']\]$/, "$1");
-          if (!name || name == tAttr['ngModel']) {
-            name = tAttr['ngModel'].replace(/.*\.([^\.|\]]+)$/, "$1");
-          }
-          labelName = name + 'Label';
-          
-          // Grab the element messages.
-          messages = tElem.find('messages').remove().html();
-          
-          // Grab the label.
-          suppliedLabel = tElem.find('label');
-          
-          // Horizontal label length.
-          horizontalLabel = tAttr['horizontalLabel'];
-          
-          // The type of the element.
-          type = tAttr['type'];
-          if (typeof type === 'undefined') {
-            type = "text";
-          }
-          
-          // Columns.
-          cols = tAttr['cols'];
-          if (typeof cols === 'undefined') {
-            // Default to 12 columns.
-            cols = 12;
-          }
-          
-          // Template type.
-          templateType = type.toLowerCase();
-          if (templateType === 'date') {
-            templateType = 'date';
-          }
+        compile: function( iElem, iAttr ) {
           
           // Return the linking function.
-          return function ($scope, iElem, iAttr, form) {
-            // Import the correct template depending on the type.
-            var template = 'components/kint-ui/directives/widgets/' + theme + '/input' + (horizontalLabel ? '-horizontal' : '' ) + '.html';
-            $templateRequest(template).then(function(html) {
+          return {
+            pre: function ($scope, iElem, iAttr, form) {
+              // Generate the name and label id from the supplied props.
+              name = extractProperty(iAttr['ngModel']);
+              labelName = name + 'Label';
               
-              // Grab the template as a jQuery type object which will allow us to modify it.
-              var template = angular.element(html);
-              
-              // Set the label.
-              var label = template.find('label').text ( suppliedLabel.remove().text() ); // .replace(template.find('label'));
-              label
-                .attr('aria-label', label.text())
-                .attr('id', labelName)
-                .attr('for', name);
-              
-              // Input element.
-              var inputEl = template.find('input')
-                .attr('name', name)
-                .attr('id', name)
-                .attr('aria-labelledby', labelName)
-                .attr('type', type);
-              
-              if (horizontalLabel) {
-                
-                // Need to add the classes to the correct elements.
-                var componentWidth = cols - horizontalLabel;
-                label.addClass('span-' + horizontalLabel);
-                inputEl.parent().addClass('span-' + componentWidth);
+              // Grab the element messages.
+              messages = iElem.find('messages');
+              if (messages.length > 0) {
+                messages = messages.remove();
               } else {
-                
-                // Just add to the root element.
-                template.addClass("span-" + cols);
+                messages = false;
               }
               
-              // Add all the attributes supplied to the input element.
-              addAttributes(tAttr, inputEl);
+              // Grab the label.
+              suppliedLabel = iElem.find('label');
               
-              // Add the messages.
-              template.find('.messages-block').html(messages);
+              // Horizontal label length.
+              horizontalLabel = iAttr['horizontalLabel'];
               
-              // Create a scope that inherits from the current here. This allows us to bind the,
-              // form controller and other needed parts for the templates, while preserving the
-              // existing bindings.
-              var privateScope = $scope.$new(false);
+              // The type of the element.
+              type = iAttr['type'];
+              if (typeof type === 'undefined') {
+                type = "text";
+              }
               
-              // Add the form to the scope for referencing in our template.
-              privateScope.$form = form;
-              privateScope.property = name;
+              switch ( type ) {
+                case "date" :
+                  templateType = type;
+                  type = "text";
+                  break;
+                case "ref-data" :
+                case "ref-object" :
+                  templateType = type;
+                  
+                  // Special directives seperate the model into "object" and "property"
+                  iAttr['object'] = extractObject(iAttr['ngModel']);
+                  iAttr['property'] = name;
+                  
+                  // Remove the model to stop it being bound in the compile phase.
+                  delete iAttr['ngModel'];
+              }
               
-              // Replace the content.
-              iElem.replaceWith(template);
-              
-              // Compile the template.
-              $compile(template)(privateScope);
-            });
+              // Columns.
+              cols = iAttr['cols'];
+              if (typeof cols === 'undefined') {
+                // Default to 12 columns.
+                cols = 12;
+              }
+            },
+            post: function ($scope, iElem, iAttr, form) {
+              // Import the correct template depending on the type.
+              var template = 'components/kint-ui/directives/widgets/' + theme + '/' + templateType + '.html';
+              $templateRequest(template).then(function(html) {
+                
+                // Grab the template as a jQuery type object which will allow us to modify it.
+                var template = angular.element(html);
+                
+                // Set the label.
+                var label = template.find('label').text ( suppliedLabel.remove().text() ); // .replace(template.find('label'));
+                label
+                  .attr('aria-label', label.text())
+                  .attr('id', labelName)
+                  .attr('for', name);
+                
+                // Input element.
+                var inputEl = template.find(templateType)
+                  .attr('name', name)
+                  .attr('id', name)
+                  .attr('aria-labelledby', labelName)
+                  
+                // Use the supplied type as a subtype.
+                if (templateType === 'input') {
+                  inputEl.attr('type', type);
+                }
+                
+                if (horizontalLabel) {
+                  
+                  // Need to add the classes to the correct elements.
+                  var componentWidth = cols - horizontalLabel;
+                  label.addClass('span-' + horizontalLabel);
+                  inputEl.parent().addClass('span-' + componentWidth);
+                } else {
+                  
+                  // Just add to the root element.
+                  template.addClass("span-" + cols);
+                }
+                
+                // Add all the attributes supplied to the input element.
+                addAttributes(iAttr, inputEl);
+                
+                // Add the messages.
+                var messageBlock = template.find('.messages-block');
+                if (messageBlock.length > 0) {
+                  if (messages === false) {
+                    // No messages, we should remove the element from the template.
+                    template.find('.messages-block').html(messages);
+                  } else {
+                    messageBlock.html(messages);
+                  }
+                }
+                
+                // Create a scope that inherits from the current here. This allows us to bind the,
+                // form controller and other needed parts for the templates, while preserving the
+                // existing bindings.
+                var privateScope = $scope.$new(false);
+                
+                // Add the form to the scope for referencing in our template.
+                privateScope.$form = form;
+                privateScope.property = name;
+                
+                // Replace the content.
+                iElem.replaceWith(template);
+                
+                // Compile the template.
+                $compile(template)(privateScope);
+              });
+            }
           };
         },
       };
