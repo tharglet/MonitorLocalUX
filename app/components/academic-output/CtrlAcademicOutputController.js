dd9e07fb71c00b043b452fa137a4e1589f88bbdd
Ian Ibbotson <ianibbo@gmail.com>
2016-04-19 12:25:01 +0100

    Adding doi modal

--- app/components/academic-output/CtrlAcademicOutputController.js
+++ app/components/academic-output/CtrlAcademicOutputController.js
@@ -3,12 +3,19 @@
 define (
   ['app'],
   function(app) {
-    app.registerController('AcademicOutputController', [ '$scope', function($scope) {
+    app.registerController('AcademicOutputController', [ '$scope', '$modal', function($scope, $modal) {
 
       console.log("AcademicOutputController");
 
       $scope.lookupDOI = function() {
         console.log("lookupDOI");
+          var modalInstance = $modal.open({
+          // animation: $scope.animationsEnabled,
+          templateUrl: 'academic_output/partials/_doi.html',
+          // controller: 'DOILookupCtrl',
+          // size: size ,
+        });
+
       };
 
       /**
