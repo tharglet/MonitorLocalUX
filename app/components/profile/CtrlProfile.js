1571e1f9a9340d5b9773487a0e38484a09163da7
Ian Ibbotson <ian.ibbotson@gmail.com>
2016-06-28 13:03:06 +0100

    updated

--- app/components/profile/CtrlProfile.js
+++ app/components/profile/CtrlProfile.js
@@ -6,6 +6,11 @@ define (
     app.registerController('ctrlProfile', [ '$scope', function($scope) {
 
       console.log("ProfileController");
+      console.log("User:%o",$scope.application.user);
+
+      $scope.requestAffiliation = function() {
+        console.log("requestAffiliation");
+      }
 
     }]);
   }
