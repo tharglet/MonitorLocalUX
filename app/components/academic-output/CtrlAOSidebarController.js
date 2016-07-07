7ac1f2c36753b5566d153c47e7ebcb122f9bc351
Mike Campbell <mike.campbell@k-int.co.uk>
2016-07-07 15:39:02 +0100

    Changed 'Add an ISSN/ eISSN' to 'Add a publisher'

--- app/components/academic-output/CtrlAOSidebarController.js
+++ app/components/academic-output/CtrlAOSidebarController.js
@@ -4,10 +4,10 @@ define (
   ['app'],
   function(app) {
     app.registerController('AOSidebarController', [ '$scope', 'context', 'grailsResource', 'debounce', function ($scope, context, resource, debounce) {
-      
+
       // The context will not exist in this scope. We need it for watches.
       $scope.context = context;
-      
+
       // Use the contextual resource and check the rules for workflow.
       $scope.workflow = {};
       var workflowGrouping = {
@@ -23,13 +23,13 @@ define (
           'Publication' : [
             'Add an identifier (DOI/PMID/PMCID)',
             'Add a journal/conference title',
-            'Add an ISSN or eISSN'
+            'Add a publisher'
           ],
           'Finance' : [
             'Add a cost item for the actual expenditure'
           ]
       };
-      
+
       // Grab the workflow status.
       var refreshRules = debounce(function () {
         resource.checkRules({ id: 'workflow' }, context).$promise.then(function (workflowData) {
@@ -38,19 +38,19 @@ define (
             if ( typeof wf[group] == 'undefined' ) {
               wf[group] = {};
             }
-  
+
             angular.forEach(rules, function (rule) {
               wf[group][rule] = workflowData[rule];
             });
           });
         });
       }, 500);
-      
+
       // Shallow watches. Only change if reference changes not the properties.
       $scope.$watchGroup(
         ['context.name','context.publicationRoute', 'context.publicationTitle', 'context.apcFundingApproval'], refreshRules
       );
-      
+
       // Deep watches. Watch for items added to the collection as well as properties of each items changing.
       $scope.$watch('context.identifiers', refreshRules, true);
       $scope.$watch('context.academicOutputCosts', refreshRules, true);
