cbefa821addbff2801c39b3344f2f569ab66ef2e
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2016-07-13 12:10:01 +0100

    Fix for calcs from exchange rates.

--- app/components/invoice/CtrlCostItemController.js
+++ app/components/invoice/CtrlCostItemController.js
@@ -9,7 +9,7 @@ define (
       $scope.updateBaseCurrencyValue = function () {
         
         // Grab the rate.
-        var rate = $scope.application.settings.currency.rates[$scope.costItem.currency].rate;
+        var rate = $scope.application.currency.rates[$scope.costItem.currency].rate;
         
         // Update the value of GBP
         $scope.costItem.grossValueGBP.value = parseFloat((rate * $scope.costItem.grossValue.value).toFixed($scope.costItem.grossValueGBP.decimals));
