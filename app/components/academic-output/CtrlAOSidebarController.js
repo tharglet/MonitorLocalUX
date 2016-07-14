5a36a21deb9e16844591e751d4606263ec777d6c
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2016-07-14 12:32:01 +0100

    Just remove compliance rule for now.

--- app/components/academic-output/CtrlAOSidebarController.js
+++ app/components/academic-output/CtrlAOSidebarController.js
@@ -27,9 +27,6 @@ define (
           ],
           'Finance' : [
             'Add a cost item for the actual expenditure'
-          ],
-          'Compliance' : [
-            'Compliance checks for review'
           ]
       };
 
@@ -58,7 +55,6 @@ define (
       $scope.$watch('context.identifiers', refreshRules, true);
       $scope.$watch('context.academicOutputCosts', refreshRules, true);
       $scope.$watch('context.funds', refreshRules, true);
-      $scope.$watch('context.complianceRules', refreshRules, true);
     }]);
   }
 );
