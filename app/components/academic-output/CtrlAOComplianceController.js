0667148110bc45d6cfdafbceeb99d3caf85d9397
Steve Osguthorpe <steve.osguthorpe@k-int.com>
2016-08-12 10:21:35 +0100

    Merging compliance and workflow into one controller.

--- app/components/academic-output/CtrlAOComplianceController.js
+++ app/components/academic-output/CtrlAOComplianceController.js
@@ -3,32 +3,14 @@
 define (
   ['app'],
   function(app) {
-    app.registerController('AOComplianceController', [ '$scope', 'context', 'grailsResource', 'debounce', function ($scope, context, resource, debounce) {
-        
-      // Let's store this against the context object we also need a view of the compliance data,
-      // which might not be available yet.
-      if (typeof $scope.context.$$compliance === 'undefined') {
+    app.registerController('AOComplianceController', [ '$scope', function ($scope) {
+      
+      if (!$scope.context.$$compliance) {
         $scope.context.$$compliance = {};
       }
       
-      // Scope value.
+      // Just create the pointer for easier access in the views.
       $scope.compliance = $scope.context.$$compliance;
-      
-      var refreshRules = debounce(function () {
-        resource.checkRules ({ id: null }, context).$promise.then(
-          function (complianceData) {
-            angular.copy(complianceData, $scope.compliance);
-          }
-        );
-      }, 20);
-  
-      // Shallow watches. Only change if reference changes not the properties.
-      $scope.$watchGroup(
-        ['context.publicationRoute', 'context.embargoPeriod','context.licence', 'context.acknowledgement', 'context.accessStatement'], refreshRules
-      );
-      
-      // Deep watches. Watch for items added to the collection as well as properties of each items changing.
-      $scope.$watch('context.deposits', refreshRules, true);
     }]);
   }
 );
